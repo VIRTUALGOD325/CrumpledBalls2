523212043fb71bb597852afcb4ed694d62c563e4
VIRTUALGOD325 <65289865+VIRTUALGOD325@users.noreply.github.com>
2020-07-27 15:02:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,64 @@
+
+var ball, ballIMG;
+var dustbinIMG;
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	ballIMG = loadImage("paper.png");
+	dropGroundIMG = loadImage("dustbingreen.png");
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+	ball=createSprite(width/2, 120, 10,10);
+	ball.addImage(ballIMG);
+	ball.scale = 0.6;
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	ball = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
+
+	groundSprite=createSprite(width/2, height-35, width,10);
+	groundSprite.shapeColor=color(255)
+
+	dropGround = createSprite(width/1.65, 570, 20,100);
+	dropGround.addImage(dropGroundIMG);
+	dropGround.scale = 0.6;
+
+
+	World.add(world, ball);
+
+	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
+ 	World.add(world, ground);
+
+
+	Engine.run(engine);
+
+	keyPressed();
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(1);
+  
+  drawSprites();
+ 
+}
+
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		ball.velocityY = 3;
+	}
+}
+
+
